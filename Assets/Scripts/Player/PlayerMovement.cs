d6dfe889748b493a2fd91cc1ea1c892e4fa568ee
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2021-01-29 22:45:30 +0000

    I've made our level 1 basics
    Simple movement script and assets imported. Level 1 is done on a visual level

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private Vector2 movement;
+    [SerializeField] [Range(1f,100f)] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movement * Time.deltaTime * speed;
+    }
+}
