c32e08750584ffbf4eb1673e29e98508f9e2ce6a
Indibulous <59960214+Indibulous@users.noreply.github.com>
2021-01-30 09:29:03 +0000

    Made grid-based movement prototype
    Collisions don't work yet, and you can move diagonally, which I'm not sure we want, but apart from that I think  it works pretty well.

--- Assets/Scripts/scripts.cs
+++ Assets/Scripts/scripts.cs
@@ -8,29 +8,47 @@ public class scripts : MonoBehaviour
     public Rigidbody2D rb;
 
     public Vector2 movement;
-    public float movementSpeed;
+    public float moveSpeed;
+    public bool isMoving;
+    public Vector2 moveStart;
+    public Vector2 moveEnd;
+    public float moveTimer;
+    public float moveWait;
+    public Vector2 gridSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        movementSpeed = 10;
+        moveSpeed = 8;
+        moveWait = 1.45f;
+        gridSize = new Vector2(0.5f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("space"))
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+	    if ((movement.x != 0 || movement.y != 0) && isMoving == false)
         {
-            movement = new Vector2(1, 0);
+            moveTimer = 0;
+            moveStart = rb.position;
+            moveEnd = rb.position + (movement * gridSize);
+            isMoving = true;
         }
-        else
+        if (moveTimer > moveWait)
         {
-            movement = new Vector2 (0,0);
+            isMoving = false;
         }
+ 
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = movement * Time.deltaTime * movementSpeed;
+        if (isMoving == true)
+        {
+            moveTimer += Time.deltaTime * moveSpeed;
+            rb.MovePosition(new Vector2(Mathf.Lerp(moveStart.x, moveEnd.x, Mathf.Clamp(moveTimer,0,1)), Mathf.Lerp(moveStart.y, moveEnd.y, Mathf.Clamp(moveTimer,0,1))));
+        }
     }
 }
