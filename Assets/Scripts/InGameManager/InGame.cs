bfeebe2fa297bd97910872a96a15f9cbd3b7c4e3
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2021-01-31 12:22:57 +0000

    Pop up menu works, new scenes
    I've made the esc pop up menu, fail and win pop up. I've also made a few quick levels just in case. They are not refined levels

--- Assets/Scripts/InGameManager/InGame.cs
+++ Assets/Scripts/InGameManager/InGame.cs
@@ -1,56 +1,93 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class InGame : MonoBehaviour
 {
+
+    public GameObject popUp;
+    public GameObject winMenu;
+    public GameObject loseMenu;
+    private bool otherMenu;
+    private Scene currentScene;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        otherMenu = false;
+        currentScene = SceneManager.GetActiveScene();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && otherMenu == false)
         {
-            //restart scene
+            RetryLevel();
         }
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && otherMenu == false)
         {
-            OpenMenu();
+            ToggleMenu();
+            Debug.Log("Escape");
         }
     }
 
     /// <summary>
     /// OpenMenu should toggle the popup menu screen
     /// </summary>
-    public void OpenMenu()
+    public void ToggleMenu()
     {
-
+        if (popUp.gameObject.activeSelf)
+        {
+            popUp.gameObject.SetActive(false);
+        }
+        else
+        {
+            popUp.gameObject.SetActive(true);
+        }
     }
 
     public void PlayerDied()
     {
-        //pop up dead menu
+        otherMenu = true;
+        if (!loseMenu.gameObject.activeSelf)
+        {
+            loseMenu.gameObject.SetActive(true);
+        }
+        if (winMenu.gameObject.activeSelf)
+        {
+            winMenu.gameObject.SetActive(false);
+        }
     }
 
     public void PlayerWin()
     {
-        //pop up re-united menu
+        otherMenu = true;
+        if (!winMenu.gameObject.activeSelf)
+        {
+            winMenu.gameObject.SetActive(true);
+        }
+        if (loseMenu.gameObject.activeSelf)
+        {
+            loseMenu.gameObject.SetActive(false);
+        }
     }
 
     public void ReturnToMenu()
     {
-
+        SceneManager.LoadScene("MainMenu");
     }
     public void GoToNextLevel()
     {
-
+        if (currentScene.buildIndex < SceneManager.sceneCountInBuildSettings -1)
+        {
+            SceneManager.LoadScene(currentScene.buildIndex + 1);
+        }
     }
     public void RetryLevel()
     {
-
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
     
 
