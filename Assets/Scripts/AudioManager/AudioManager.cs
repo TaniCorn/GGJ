a8b9aceb9a734d4372f5ad9e3e79bfa1353d058c
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2021-01-31 14:01:54 +0000

    Many developments
    The level select menu now works, and all the levels can now progress into eachother. I have made a audiomanager that plays as soon as the game loads and persists into the next games. I have also fixed a bug where the player can move while the pause menu is active.

---
+++ Assets/Scripts/AudioManager/AudioManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClips[] aud;
+    public AudioSource audioSource;
+
+    [System.Serializable]
+    public class AudioClips
+    {
+        public string clipName;
+        public AudioClip audioClip;
+        public float clipVolume = 1;
+        public bool loop;
+
+        public AudioSource audioSource;
+    };
+
+    public static AudioManager existant;
+
+    private void Awake()
+    {
+
+        if (existant == null)
+        {
+            existant = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+        foreach (AudioClips audio in aud)
+        {
+            audio.audioSource = gameObject.AddComponent<AudioSource>();
+            audio.audioSource.clip = audio.audioClip;
+
+            audio.audioSource.volume = audio.clipVolume;
+            audio.audioSource.loop = audio.loop;
+        }
+    }
+
+    private void Start()
+    {
+        PlaySound("Music");
+    }
+
+    public void PlaySound(string audioName)
+    {
+        AudioClips audioFound = Array.Find(aud, AudioClips => AudioClips.clipName == audioName);
+        if (audioFound != null)
+        {
+            audioFound.audioSource.Play();
+        }
+    }
+}
